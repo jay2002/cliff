a5d40d5f53fa0539553989607e1a91af07734eb7
Tiancheng Zhi <zhitiancheng1225@163.com>
2015-11-05 13:44:04 +0800

    update comments

--- cliff/selection.py
+++ cliff/selection.py
@@ -8,8 +8,10 @@ def select_preceding(features, k):
     :param k: preceding k features or proposals for each image are selected
     :type k: integer
     :return: selected features or proposals
-    :rtype: list, each element is a k-by-m ndarray, where m is feature
-        dimension or 4 for proposals
+    :rtype: list. Each element is a k'-by-m ndarray, where m is feature
+        dimension or 4 for proposals. If there are enough features or proposals
+        for selection, then k' = k, else all features or proposals are
+        selected.
     """
 
     return [i[:k] for i in features]
