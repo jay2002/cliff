e754d0256b89328170f8b57e6fb4952a68677ce1
Tiancheng Zhi <zhitiancheng1225@163.com>
2015-11-10 10:57:00 +0800

    fix bug

--- cliff/normalization.py
+++ cliff/normalization.py
@@ -2,9 +2,12 @@
     This module includes normalization functions
 """
 
+# pylint: disable=E1101
+
 import numpy as np
 
-def l2(features):
+
+def l2_norm(features):
     """ l2 normalization
     :param features: features before normalization
     :type features: list
@@ -15,6 +18,7 @@ def l2(features):
     transformed_feature = []
     for i in features:
         tmp = i.copy()
-        tmp = tmp / np.sqrt(np.dot(tmp, tmp))
+        for j in range(tmp.shape[0]):
+            tmp[j] = tmp[j] / np.sqrt(np.dot(tmp[j], tmp[j]))
         transformed_feature.append(tmp)
     return transformed_feature
